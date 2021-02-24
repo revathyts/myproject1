136b856e987bc2baaab321a11e890604a1306919
revathyts <79398169+revathyts@users.noreply.github.com>
2021-02-24 11:42:40 +0530

    Update registration.php

--- application/views/registration.php
+++ application/views/registration.php
@@ -1,65 +1,48 @@
-<!DOCTYPE html>
+<! DOCTYPE html>
 <html>
 <head>
-<title> form </title>
+<title>  registeration </title>
 <style>
 h1{
     color:red;
     text-decoration: underline;
     text-align: center;
   }
-  table{
-  margin-top:80px;
-  border:1px solid black;
-  border-collapse: collapse;
-  background-color:rgba(0,0,0,0.4);
-  width:400px;
-  height:auto;
-  margin-left:400px;
-  }
-  td{
-  text-align:center;
-  padding:5px;
-  margin:10px;
-  color:white;
-  
+input{
+padding:10px;
+margin:10px;
 }
-
-
-
-
-</style>
+fieldset
+{
+margin-left: 500px;
+text-align: center;
+background-color: rgba(0,0,0,0.8);
+}
+ </style>
 </head>
 <body>
-  
-<h1> Registeration Form</h1>
-<form method="post" action="<?php echo base_url()?>main/reg_access">
-<table>
-<tr>
-  <td>First Name</td>
-  <td><input type="text" name="fname"placeholder="First Name"></td></tr>
-  <tr>
-  <td>Last Name</td>
-  <td><input type="text" name="lname"placeholder="Last Name"></td></tr>
-<tr>
-  <td>Username</td>
-  <td><input type="text" name="uname"placeholder="uname"></td></tr>
- <tr>
-  <td>Password</td>
-  <td><input type="password" name="password"placeholder="password"></td></tr>
-<tr>
-  <td>Mobile</td>
-  <td><input type="text" name="mobile"placeholder="Mobile number">
-</td></tr>
-<tr>
-  <td>Email</td>
-  <td><input type="email" name="email"placeholder="email">
-</td></tr>
+  <h1> Registeration Form</h1>
+ <!---form starts---->
+ <fieldset style="width:20%;"  >
+  <form name="myform" action="<?php echo base_url()?>main/reg_access" method="POST" >
+       
+        <input type="text" name="fname"  placeholder="firstname" pattern=".{3,}"   required title="3 characters minimum" maxlength="25"></br>
+     
+      <input type="text" name="lname"  placeholder="lastname" pattern=".{3,}"   required title="3 characters minimum"  maxlength="25"></br>
+           
+            <input type="text" name="uname"  placeholder="username" required pattern=".{3,}"   required title="3 characters minimum" maxlength="10"></br>
 
-<tr><td><input type="submit" class="btn" value="REGISTER">
-</td></tr>
-</table>
-</form>
+<input type="password" name="password" placeholder="password" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"> </br>
+       
+        <input type="text" name="mobile"  placeholder="mobile no" required minlength="10"maxlength="10"></br>
+     
+      <input type="email" name="email"  placeholder="email" required> </br>
+   
+    <input type="submit" name="submit" >
+            </form>
+            </fieldset>
+ <!----form ends----->
+ 
 
 </body>
 </html>
